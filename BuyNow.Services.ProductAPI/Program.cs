95baa9f5556c4b95f3412178e1798bd0f66cdf23
t0118430 <antonio.rocha.t0118430@edu.atec.pt>
2023-03-16 12:04:11 +0000

    Configured Swagger

--- BuyNow.Services.ProductAPI/Program.cs
+++ BuyNow.Services.ProductAPI/Program.cs
@@ -4,6 +4,7 @@ using BuyNow.Services.ProductAPI.DbContexts;
 using BuyNow.Services.ProductAPI.Models;
 using BuyNow.Services.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,10 +15,19 @@ IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "BuyNow.Services.ProductAPI", Version = "v1"});
+});
 var app = builder.Build();
 
 app.UseRouting();
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuyNow.Services.ProductAPI v1");
+    c.RoutePrefix = "";
+});
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
